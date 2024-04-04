80e6afb62cc833ad35ed8f9c88fa1c969042960b
tyson-tyler <benword25@gmail.com>
2024-04-04 16:48:34 +0530

    push

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -72,30 +72,25 @@ const MeetingTypeList = () => {
   return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
       <HomeCard
-        img="/icons/add-meeting.svg"
-        title="New Meeting"
-        description="Start an instant meeting"
+        img="/icons/add-metting.svg"
+        title="Create Meeting"
+        description="Start an a meeting"
         handleClick={() => setMeetingState('isInstantMeeting')}
+        className='transition hover:scale-105 cursor-pointer bg-purple-600'
       />
       <HomeCard
         img="/icons/join-meeting.svg"
         title="Join Meeting"
         description="via invitation link"
-        className="bg-blue-1"
+        className="bg-red-500 transition hover:scale-105 cursor-pointer"
         handleClick={() => setMeetingState('isJoiningMeeting')}
       />
-      <HomeCard
-        img="/icons/schedule.svg"
-        title="Schedule Meeting"
-        description="Plan your meeting"
-        className="bg-purple-1"
-        handleClick={() => setMeetingState('isScheduleMeeting')}
-      />
+   
       <HomeCard
         img="/icons/recordings.svg"
         title="View Recordings"
         description="Meeting Recordings"
-        className="bg-red-600"
+        className="bg-green-600 transition hover:scale-105 cursor-pointer"
         handleClick={() => router.push('/recordings')}
       />
 
